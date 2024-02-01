4e26e765ca653077e0f63ec9887eb7e9b1ecd119
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-02-01 15:33:10 +0300

    8.ders sonu

---
+++ Controllers/StatisticController.cs
@@ -0,0 +1,27 @@
+﻿using MeyawoPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeyawoPortfolio.Controllers
+{
+    public class StatisticController : Controller
+    {
+        DbMyPortfolioEntities1 db = new DbMyPortfolioEntities1();
+        public ActionResult Index()
+        {
+            //Aggregate functions --> Sum Avg Count Min Max
+
+            ViewBag.categoryCount = db.TblCategory.Count();
+            ViewBag.projectCount = db.TblProject.Count();
+            ViewBag.MessageCount = db.TblContact.Count();
+            ViewBag.FlutterProjectCount = db.TblProject.Where(x => x.ProjectCategory == 1).Count();
+            ViewBag.isReadCount = db.TblContact.Where(x=> x.IsRead == false).Count();
+            //ViewBag.lastProject = db.TblProject.OrderByDescending(x => x.ProjectId).First();
+            ViewBag.lastProject = db.LastProjectName().FirstOrDefault();
+            return View();
+        }
+    }
+}
